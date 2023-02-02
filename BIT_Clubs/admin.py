8b641abae153191d7cffa574c704462657dc54f3
Shashank Yadav <106250393+Shanks2121@users.noreply.github.com>
2023-02-03 01:07:15 +0530

    Deleted files

--- BIT_Clubs/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Club, TeamMember
-
-admin.site.register(Club)
-admin.site.register(TeamMember)
--- BIT_Clubs/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BitClubsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'BIT_Clubs'
